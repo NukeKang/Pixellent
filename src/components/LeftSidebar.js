952e3abc77f42811ec1dff80fb9009b6a7cd3ab5
rkdalstjd224 <rkdalstjd224@gmail.com>
2022-07-12 21:06:33 +0900

    feat: 새로운 프로젝트 시작 버튼

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -4,12 +4,16 @@ import Bucket from './Bucket';
 import DownloadImage from './DownloadImage';
 import Eraser from './Eraser';
 import Eyedropper from './Eyedropper';
+import NewProject from './NewProject';
 import Paint from './Paint';
 import Pallette from './Pallette';
 
 const LeftSidebar = () => {
   return (
     <>
+      <div>
+        <NewProject />
+      </div>
       <div>
         <Paint />
         <Eraser />
---
+++ src/components/NewProject.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import useStore from '../store/store';
+import makeArray from '../utils/makeArray';
+
+import Button from './common/Button';
+
+const NewProject = () => {
+  const { row, column, baseColor } = useStore();
+  const handleClick = () => {
+    useStore.setState({ canvas: makeArray(row, column, baseColor) });
+  };
+  return <Button onClick={handleClick}>NEW</Button>;
+};
+
+export default NewProject;
